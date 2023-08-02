a88624f8f2406798062f069064bd25c88d1215c5
Delta <lisa2edit@gmial.com>
2023-08-02 11:00:55 -0700

    Here we go

--- read_it_filter.py
+++ read_it_filter.py
@@ -1,4 +1,4 @@
-from suncraft_parsing import part, line, items_block, live_url, admin_url, import_handeling, catsubtag_block, tags, subcat, cat, name, description, sort_rank, string_it, file_saving
+from suncraft_parsing import upc, part, line, meta_name, meta_data, items_block, live_url, admin_url, import_handeling, catsubtag_block, tags, subcat, cat, name, description, sort_rank, string_it, file_saving
 from suncraft_varriables import suncraft_database_file, looking_for, type_of_group
 
 # Variables
@@ -29,33 +29,43 @@ for pages_id in sorted(imported_dict, key = lambda pages_id: int(imported_dict[p
         any_of_these = i
 
         if any_of_these in this_group:
-                all_cat.update(cat(imported_dict, pages_id, False))
-                all_subcat.update(subcat(imported_dict, pages_id, False))
-                all_tags.update(tags(imported_dict, pages_id, False))
+            all_cat.update(cat(imported_dict, pages_id, False))
+            all_subcat.update(subcat(imported_dict, pages_id, False))
+            all_tags.update(tags(imported_dict, pages_id, False))
 
+            meta_name_list = meta_name(imported_dict, pages_id, False, "- ")
+            meta_data_list = meta_data(imported_dict, pages_id, False)
+            meta_dict = dict(zip(meta_name_list, meta_data_list))
+
+
+            upc_list = upc(imported_dict, pages_id, False)
+            part_list = part(imported_dict, pages_id, False)
+            parts_dict = dict(zip(part_list, upc_list))
                 #Page
-                product_page += f'{name(imported_dict, pages_id)}\n'
-                product_page += f'{imported_dict[pages_id]["slug_product"]}\n'
-                product_page += f'{sort_rank(imported_dict, pages_id)}\n'
-                product_page += f'{admin_url(pages_id)}\n'
-                product_page += f'{live_url(imported_dict, pages_id)}\n'
-                product_page += f'\n'
+                # product_page += f'{name(imported_dict, pages_id)}\n'
+                # product_page += f'{imported_dict[pages_id]["slug_product"]}\n'
+                # product_page += f'{sort_rank(imported_dict, pages_id)}\n'
+                # product_page += f'{admin_url(pages_id)}\n'
+                # product_page += f'{live_url(imported_dict, pages_id)}\n'
+                # product_page += f'\n'
 
-                product_page += \
-                    line(30) +\
-                    f'Name: {name(imported_dict, pages_id)}\n' +\
-                    line(30) +\
-                    catsubtag_block(imported_dict, pages_id) +\
-                    line(30) +\
-                    f'Description: {description(imported_dict, pages_id)}' + "\n" +\
-                    line(30) +\
-                    f'| UPC | {str.center("Part", part_name_length)}|  Info\n' +\
-                    line(30)
+            product_page += \
+                f'{name(imported_dict, pages_id)}\n' +\
+                f'{catsubtag_block(imported_dict, pages_id)}\n' +\
+                f'{description(imported_dict, pages_id)}\n' +\
+                f'{line(30)}\n' +\
+                f'| UPC | {str.center("Part", part_name_length)}|  Info\n' +\
+                f''
+            for i in parts_dict:
+                try:
+                    product_page += (f'|{parts_dict[i]}| {str.rjust(i + "|", part_name_length + 1)} {meta_dict[i]}\n')
+                except KeyError:
+                    product_page += (f'|{parts_dict[i]}| {str.rjust(i + "|", part_name_length + 1)}\n')
 
     body += product_page 
-body += "Categories: " + string_it(all_cat) + "\n"
-body += "Subcategories: " + string_it(all_subcat) + "\n"
-body += "Tags: " + string_it(all_tags) + "\n"
+# body += "Categories: " + string_it(all_cat) + "\n"
+# body += "Subcategories: " + string_it(all_subcat) + "\n"
+# body += "Tags: " + string_it(all_tags) + "\n"
 
 
 # Creates and/or opens the file to work with.
