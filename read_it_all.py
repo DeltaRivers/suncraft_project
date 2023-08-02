01ce7a8d5314ac9ed0824afa65ab1ad737b222ad
Delta <lisa2edit@gmial.com>
2023-08-01 20:03:05 -0700

    Cleaned it up

--- read_it_all.py
+++ read_it_all.py
@@ -1,10 +1,13 @@
-from suncraft_parsing import items_block, live_url, admin_url, import_handeling, catsubtag_block, tags, subcat, cat, name, description, sort_rank, string_it, file_saving, meta_data, meta_name, meta_name2, part, upc
+from suncraft_parsing import admin_url, import_handeling, catsubtag_block, tags, subcat, cat, name, description, string_it, file_saving, meta_data, meta_name2, part, upc
 from suncraft_varriables import suncraft_database_file, looking_for, type_of_group
 
 # Variables
 file_name_prefix = "all"
 imported_dict = import_handeling(suncraft_database_file, False)
 body = ""
+def line(number, segment = "-"):
+    return (segment * number) + "\n"
+    
 all_cat = set()
 all_subcat = set()
 all_tags = set()
@@ -13,7 +16,14 @@ all_tags = set()
 # This part is the body of the document and how it's formatted
 for pages_id in sorted(imported_dict, key = lambda pages_id: int(imported_dict[pages_id]["rank"])):
     product_page = "" # This holds the information for this loop don't remove it
-    
+    part_name_length = 0
+    for i in part(imported_dict, pages_id, False):
+        if len(i) > part_name_length:
+            part_name_length = len(i)
+
+    all_cat.update(cat(imported_dict, pages_id, False))
+    all_subcat.update(subcat(imported_dict, pages_id, False))
+    all_tags.update(tags(imported_dict, pages_id, False))
 
     meta_name_list = meta_name2(imported_dict, pages_id, False, "- .")
     meta_data_list = meta_data(imported_dict, pages_id, False)
@@ -23,32 +33,23 @@ for pages_id in sorted(imported_dict, key = lambda pages_id: int(imported_dict[p
     part_list = part(imported_dict, pages_id, False)
     parts_dict = dict(zip(part_list, upc_list))
 
-    all_cat.update(cat(imported_dict, pages_id, False))
-    all_subcat.update(subcat(imported_dict, pages_id, False))
-    all_tags.update(tags(imported_dict, pages_id, False))
 
     #Page
-    product_page += ("=" * 30) + "\n"
-    product_page += f'{name(imported_dict, pages_id)}\n'
-    product_page += ("=" * 30) + "\n"
-    product_page += catsubtag_block(imported_dict, pages_id)
-    product_page += ("=" * 30) + "\n"
-    product_page += description(imported_dict, pages_id) + "\n"
-    product_page += ("-" * 30) + "\n"
-    xx = 0
-    for i in part(imported_dict, pages_id, False):
-        if len(i) > xx:
-            xx = len(i)
-    product_page += f'| UPC | {str.center("Part", xx)}|  Info\n'
-    product_page += ("-" * 30) + "\n"
+    product_page += line(30) +\
+        f'{name(imported_dict, pages_id)}\n' +\
+        line(30) +\
+        catsubtag_block(imported_dict, pages_id) +\
+        line(30) +\
+        description(imported_dict, pages_id) + "\n" +\
+        line(30) +\
+        f'| UPC | {str.center("Part", part_name_length)}|  Info\n' +\
+        line(30)
 
     for i in parts_dict:
         try:
-            product_page += (f'|{parts_dict[i]}| {str.rjust(i + "|", xx + 1)} {meta_dict[i]}\n')
+            product_page += (f'|{parts_dict[i]}| {str.rjust(i + "|", part_name_length + 1)} {meta_dict[i]}\n')
         except KeyError:
-            product_page += (f'|{parts_dict[i]}| {str.rjust(i + "|", xx + 1)}\n')
-
-
+            product_page += (f'|{parts_dict[i]}| {str.rjust(i + "|", part_name_length + 1)}\n')
     product_page += f'\n'
     body += product_page 
 
