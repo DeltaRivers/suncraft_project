0e66197f45689a502b2a0a6f71ac86a76baa7229
Delta <lisa2edit@gmial.com>
2023-08-07 12:52:18 -0700

    took out a _

--- runit.py
+++ runit.py
@@ -2,7 +2,7 @@ from read_it_csv import read_it_csv
 from suncraft_parsing import import_handeling, file_saving_tsv 
 from suncraft_varriables import suncraft_database_file 
 
-file_name_prefix = "tsv_of_"
+file_name_prefix = "tsv_of"
 imported_dict = import_handeling(suncraft_database_file, False)
 body = read_it_csv(imported_dict)
 file_saving_tsv(f'{file_name_prefix}_{suncraft_database_file.split(".")[0]}', body)
