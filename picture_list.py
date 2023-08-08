44253a6d1770d8017cbc45bcf41ae4ddffbf25ab
Delta <lisa2edit@gmial.com>
2023-08-08 14:08:10 -0700

    Got my nice little get pic files function working

--- picture_list.py
+++ picture_list.py
@@ -1,17 +1,13 @@
-import os
 import glob
 
-path = "/Users/LisaH/Library/CloudStorage/GoogleDrive-lisah@suncraftind.com/My Drive/Product Pics All Sources/Product Images Sorted"
-dir_list = glob.glob(f"{path}/**/*.*")
+def get_pic_files_dict(\
+                        path_to_folders = "/Users/LisaH/Library/CloudStorage/GoogleDrive-lisah@suncraftind.com/My Drive/Product Pics All Sources/Product Images Sorted",\
+                        path_more = "/**/*.*"):
+    dir_list = glob.glob(path_to_folders + path_more)
+    files_dict = dict()
 
-for i in dir_list:
-#    file = 
-    print(i.split("/")[-1])
+    for i in dir_list:
+        files_dict[i.split("/")[-1].split("_")[0]] = i
+    return(files_dict)
 
-# final_list = list()
-# for i in dir_list:
-#     print(i)
-#     file_list = os.listdir(i)
-#     for i in file_list:
-#         print(i)
 
