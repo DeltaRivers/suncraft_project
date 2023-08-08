f550590012c859e5cdd4e1777bcafb3ad2a13a40
Delta <lisa2edit@gmial.com>
2023-08-08 15:32:22 -0700

    realized I could use fucntions to do exactly what they are made for and only call chunks of code when I want them.... so that's good.  XD

--- runit.py
+++ runit.py
@@ -1,8 +1,21 @@
 from read_it_csv import read_it_csv
-from suncraft_parsing import import_handeling, file_saving_tsv 
+from read_it_all import read_it_all
+from suncraft_parsing import import_handeling, file_saving_tsv, file_saving_txt
 from suncraft_varriables import suncraft_database_file 
 
-file_name_prefix = "tsv_of"
-imported_dict = import_handeling(suncraft_database_file, False)
-body = read_it_csv(imported_dict)
-file_saving_tsv(f'{file_name_prefix}_{suncraft_database_file.split(".")[0]}', body)
+def run_file_all():
+    file_name_prefix = "all"
+    imported_dict = import_handeling(suncraft_database_file, False)
+    body = read_it_all(imported_dict)
+    file_saving_txt(f'{file_name_prefix}_{suncraft_database_file.split(".")[0]}', body)
+    return()
+
+
+def run_file_csv():
+    file_name_prefix = "csv"
+    imported_dict = import_handeling(suncraft_database_file, False)
+    body = read_it_csv(imported_dict)
+    file_saving_tsv(f'{file_name_prefix}_{suncraft_database_file.split(".")[0]}', body)
+    return()
+
+
